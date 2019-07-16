53a30dea5db9fa94d774063cb5df47eac3d3a7a8
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-07-16 18:04:48 +0800

    Update GameWindow.java

--- src/Main/GameWindow.java
+++ src/Main/GameWindow.java
@@ -1,1 +1,62 @@
+package Main;
 
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class GameWindow {
+	
+	private String title="GameEngine";
+	
+	private Dimension dimension=new Dimension(600,600);
+	
+	private JFrame frame;
+	
+	private Canvas canvas;
+	
+	public GameWindow() {
+		createWindow();
+	}
+	
+	public GameWindow(Dimension dimension) {
+		this.dimension=dimension;
+		createWindow();
+	}
+	
+	public GameWindow(String title,Dimension dimension) {
+		this.title=title;
+		this.dimension=dimension;
+		createWindow();
+	}
+	
+	private void createWindow() {
+		frame=new JFrame();
+		frame.setTitle(title);
+		frame.setSize(dimension);
+		frame.setResizable(false);
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		canvas=new Canvas();
+		canvas.setPreferredSize(dimension);
+		canvas.setMaximumSize(dimension);
+		canvas.setMinimumSize(dimension);
+		
+		frame.add(canvas);
+		frame.pack();
+	}
+	
+	public Dimension getDimension() {
+		return dimension;
+	}
+	
+	public JFrame getFrame() {
+		return frame;
+	}
+	
+	public Canvas getCanvas() {
+		return canvas;
+	}
+}
