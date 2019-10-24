7dfca4ab1fbb2922cb2787e43a206c6063732755
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-10-23 22:52:21 -0700

    Update ProblemS4Tourism.java

--- src/CCCSenior2019/ProblemS4Tourism.java
+++ src/CCCSenior2019/ProblemS4Tourism.java
@@ -1,1 +1,14 @@
+package CCCSenior2019;
 
+public class ProblemS4Tourism {
+	
+	public static void main(String[]args) {
+		new ProblemS4Tourism();
+	}
+	
+	private ProblemS4Tourism() {
+		
+	}
+	
+	
+}
