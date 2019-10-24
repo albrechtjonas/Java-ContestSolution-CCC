a8742911a5c473399b9301bd28cf66b38ddc4183
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-10-23 22:52:03 -0700

    Update ProblemS3ArithmeticSquare.java

--- src/CCCSenior2019/ProblemS3ArithmeticSquare.java
+++ src/CCCSenior2019/ProblemS3ArithmeticSquare.java
@@ -1,1 +1,34 @@
+package CCCSenior2019;
 
+public class ProblemS3ArithmeticSquare {
+	
+	private String[][] grid= {{"14","X","X"},{"X","X","18"},{"X","16","X"}};
+	
+	public static void main(String[]args) {
+		new ProblemS3ArithmeticSquare();
+	}
+	
+	private ProblemS3ArithmeticSquare() {
+		for(int y=0;y<grid.length;y++) {
+			for(int x=0;x<grid[y].length;x++) {
+				if(grid[x][y].equals("X")) {
+					
+				}
+			}
+		}
+		
+		printArray();
+		
+		
+		
+	}
+	
+	private void printArray() {
+		for(int x=0;x<grid.length;x++) {
+			for(int y=0;y<grid[x].length;y++) {
+				System.out.print(grid[x][y]+",");
+			}
+			System.out.println();
+		}
+	}
+}
