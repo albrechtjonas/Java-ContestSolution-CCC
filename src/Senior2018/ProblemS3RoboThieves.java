15b254d463f404adc82eeb1df2a1e0770f19758c
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-10-25 22:11:34 -0700

    Update ProblemS3RoboThieves.java

--- src/Senior2018/ProblemS3RoboThieves.java
+++ src/Senior2018/ProblemS3RoboThieves.java
@@ -1,1 +1,11 @@
+package Senior2018;
 
+public class ProblemS3Robotvoleur {
+	public static void main(String[]args) {
+		new ProblemS3Robotvoleur();
+	}
+	
+	private ProblemS3Robotvoleur() {
+		
+	}
+}
